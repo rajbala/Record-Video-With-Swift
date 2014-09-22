c0d8fc9aa41e905bde32d97b0017ba9b2e5884e2
Raj Bala <rajbala@gmail.com>
2014-09-22 14:05:19 -0400

    Initial Commit

---
+++ Record Video With Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Record Video With Swift
+//
+//  Created by Raj Bala on 9/22/14.
+//  Copyright (c) 2014 Raj Bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Record Video With SwiftTests/Record_Video_With_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Record_Video_With_SwiftTests.swift
+//  Record Video With SwiftTests
+//
+//  Created by Raj Bala on 9/22/14.
+//  Copyright (c) 2014 Raj Bala. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Record_Video_With_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
